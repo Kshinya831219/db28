cc7b5412bb28ca1b45cb2f081507fd1c93590787
e1b22001 <e1b22001@oit.ac.jp>
2024-10-22 15:35:49 +0900

    果物データ編集, Spring Bootアプリの実装(SSE編)

--- src/main/java/oit/is/team8/db28/Db28Application.java
+++ src/main/java/oit/is/team8/db28/Db28Application.java
@@ -2,7 +2,11 @@ package oit.is.team8.db28;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableAsync
+@EnableScheduling
 @SpringBootApplication
 public class Db28Application {
 

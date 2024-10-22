e18bbe90fdf0d25806ffd716a673efb66cb9426b
Kshinya831219 <e1b22036@oit.ac.jp>
2024-10-22 13:52:37 +0900

    初期設定

---
+++ src/main/java/oit/is/team8/db28/Db28Application.java
@@ -0,0 +1,13 @@
+package oit.is.team8.db28;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Db28Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Db28Application.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/team8/db28/Db28ApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.team8.db28;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Db28ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
